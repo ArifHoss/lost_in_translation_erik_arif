ce24b0d58499fb021d2c5765482db98d8b8acd99
Erik Brandt <erik.brandt@se.experis.com>
2023-01-24 13:53:47 +0100

    test commit to dev

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ function App() {
   return (
     <div className="App">
       <h1>Welcome to Erik and Arif Translations Project!</h1>
-        <h2>Hello from Erik</h2>
+
+        <h3>test</h3>
+
     </div>
   );
 }
