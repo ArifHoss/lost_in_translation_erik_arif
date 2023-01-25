222ea526e463e6a69e145a8833f2773a6f3e9b88
Erik Brandt <erik.brandt@se.experis.com>
2023-01-25 09:02:49 +0100

    Redirecting to profile when logged in, and created context

--- src/components/Login/LoginForm.jsx
+++ src/components/Login/LoginForm.jsx
@@ -3,6 +3,7 @@ import {loginUser} from "../../api/user";
 import {useState, useEffect} from "react";
 import {storageSave} from "../../utils/storage"
 import {useNavigate} from 'react-router-dom'
+import {useUser} from "../../context/UserContext"
 
 const usernameConfig = {
     required: true,
@@ -10,7 +11,10 @@ const usernameConfig = {
 }
 const LoginForm = () => {
 
+    //Hooks
     const {register, handleSubmit, formState: {errors}} = useForm()
+    const {user, setUser} = useUser()
+    const navigate = useNavigate()
 
     // Local State
     const [loading, setLoading] = useState(false)
@@ -19,20 +23,22 @@ const LoginForm = () => {
 
     // Side Effects
     useEffect(() => {
-
-
-    }, []) //Empty deps - Only run 1ce
+        if(user !== null){
+            navigate('profile')
+        }
+    }, [user,navigate]) //Empty deps - Only run 1ce
 
     //Event Handles
     const onSubmit = async ({username}) => {
         setLoading(true)
-        const [error, user] = await loginUser(username)
+        const [error, userResponse] = await loginUser(username)
         if (error !== null) {
             setApiError(error)
         }
 
-        if (user !== null) {
-            storageSave('translation-user', user)
+        if (userResponse !== null) {
+            storageSave('translation-user', userResponse)
+            setUser(userResponse)
         }
         setLoading(false)
     }
