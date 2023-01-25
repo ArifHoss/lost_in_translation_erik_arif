949020a8429d0830556a90b8cee49f82ed28ec0f
Erik Brandt <erik.brandt@se.experis.com>
2023-01-25 16:21:48 +0100

    Updating storage and state with translations and moved to sessionstorage

--- src/components/Translation/TranslationForm.jsx
+++ src/components/Translation/TranslationForm.jsx
@@ -1,18 +1,32 @@
 import {useForm} from "react-hook-form"
 import {useUser} from "../../context/UserContext"
 import {translateAdd} from "../../api/translate"
+import {storageSave} from "../../utils/storage"
+import {STORAGE_KEY_USER} from "../../const/storageKeys"
+
 const TranslationForm = () => {
 
     const { register, handleSubmit, formState: {errors} } = useForm();
-    const {user} = useUser()
+    const {user,setUser} = useUser()
 
     const onSubmit = async ({translate}) => {
 
 
-        const [error, result] = await translateAdd(user,translate)
+        const [error, updatedUser] = await translateAdd(user,translate)
+
+
+        if (error !== null) {
+            return
+        }
+
+        //Keep UI state and server state in sync
+        storageSave(STORAGE_KEY_USER, updatedUser)
+        //upd context state
+        setUser(updatedUser)
 
         console.log('error', error)
-        console.log('result', result)
+        console.log('updatedUser', updatedUser)
+
 
     }
 
