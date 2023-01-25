a20f1339dfc8b6e1e41f202339468baf38e3cf5c
Erik Brandt <erik.brandt@se.experis.com>
2023-01-25 09:57:05 +0100

    added withAuth, redirects to login when storage is cleared

--- src/views/Translation.jsx
+++ src/views/Translation.jsx
@@ -1,6 +1,7 @@
+import withAuth from "../hoc/withAuth"
 const Translation = () => {
     return (
         <h1>Translation</h1>
     )
 }
-export default Translation
+export default withAuth(Translation)
